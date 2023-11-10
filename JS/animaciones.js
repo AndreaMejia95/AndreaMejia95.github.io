516d5bbab00d32159b464c50374f7ae756777f11
Yuri Andrea Mejia Ramirez <andreamejiar95@gmail.com>
2023-11-10 13:04:45 -0500

    Agrego base avanzada del portafolio de videojuegos

---
+++ JS/animaciones.js
@@ -0,0 +1,20 @@
+//Botón volver arriba
+const btn_scrolltop=document.querySelector('#btn_scrolltop');
+btn_scrolltop.addEventListener('click', ()=>{
+    window.scrollTo({
+        top:0,
+        behavior: "smooth"
+    });
+});
+
+window.onscroll = ()=>{
+    if(window.scrollY < 300){
+        btn_scrolltop.classList.remove("btn-scrolltop-on")
+    }else{
+        btn_scrolltop.classList.add("btn-scrolltop-on")
+    }
+};
+
+//SONIDOS CLICK
+let sonido = new Audio();
+sonido.src = "./multimedia/sonidos/botones.mp3";
